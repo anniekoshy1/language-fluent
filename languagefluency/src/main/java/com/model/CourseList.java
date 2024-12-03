df75aadc2e5c485c47fa605bdad234d5fe245291
joshnatkin <jnatkin@email.sc.edu>
2024-12-03 16:36:24 -0500

    my merge udating everytpe of progress

--- languagefluency/src/main/java/com/model/CourseList.java
+++ languagefluency/src/main/java/com/model/CourseList.java
@@ -1,6 +1,7 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class CourseList {
 
@@ -33,4 +34,33 @@ public class CourseList {
     public void saveCourses() {
         DataWriter.saveCourses();
     }
+
+public ArrayList<Course> getAvailableCourses(UUID currentCourseId) {
+    ArrayList<Course> availableCourses = new ArrayList<>();
+    for (Course course : courses) {
+        // Always add the course to the list
+        availableCourses.add(course);
+
+        // Restrict access if this is not the current course and user does not have access
+        if (!course.getId().equals(currentCourseId) && !course.getUserAccess()) {
+            System.out.println(course.getName() + " is locked. Complete the previous course to unlock.");
+        }
+    }
+    return availableCourses;
+}
+
+public void completeCourse(UUID courseId) {
+    for (int i = 0; i < courses.size(); i++) {
+        if (courses.get(i).getId().equals(courseId)) {
+            courses.get(i).setCompleted(true);
+            if (i + 1 < courses.size()) {
+                courses.get(i + 1).setUserAccess(true);
+            }
+            break;
+        }
+    }
+}
+
+
+    
 }
