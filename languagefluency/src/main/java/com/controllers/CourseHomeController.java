b859a9968b7ac73246fc9f859f433e92889903b7
dbbawa <149022838+dbbawa@users.noreply.github.com>
2024-12-04 23:48:02 -0500

    night night

--- languagefluency/src/main/java/com/controllers/CourseHomeController.java
+++ languagefluency/src/main/java/com/controllers/CourseHomeController.java
@@ -5,12 +5,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.languagefluent.App;
-import com.model.LanguageLearningFacade;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class CourseHomeController implements Initializable {
